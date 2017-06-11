818e81078e075898de7d5943f0aeb32175af570a
jyoth1raditya <jyothir_aditya@outlook.com>
2017-06-11 19:28:55 +0530

    testing applist1

--- js/framework.js
+++ js/framework.js
@@ -12,7 +12,7 @@ app.controller("search", function($scope, $http) {
   $scope.search = function() {
     if ($scope.query != '') {
       $scope.results = [];
-      $http.get("/aaa/search.json?q=" + $scope.query).
+      $http.get("http://api.susi.ai/aaa/search.json?q=" + $scope.query).
         success(function(data, status, headers, config) {
           for (var i = 0; i < data.statuses.length; i++) {
             $scope.results.push(data.statuses[i].text);
