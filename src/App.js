1de36c9d2462c11a5395600624668e3b13320a9a
DravitLochan <dlochangupta@gmail.com>
2017-06-23 00:53:53 +0530

    basics of navigation bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
+import React from 'react';
+import ReactDOM from 'react-dom';
+const NavComponent = React.createClass({
+  render: function() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <nav>
+        <div className="navWide">
+          <div className="wideDiv">
+            <a href="#">test1</a>
+            <a href="#">test2</a>
+            <a href="#">test3</a>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="navNarrow">
+          <i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
+          <div className="narrowLinks">
+            <a href="#" onClick={this.burgerToggle}>Link 1</a>
+            <a href="#" onClick={this.burgerToggle}>Link 2</a>
+            <a href="#" onClick={this.burgerToggle}>Link 3</a>
+          </div>
+        </div>
+      </nav>
     );
+  },
+  burgerToggle: function() {
+    let linksEl = document.querySelector('.narrowLinks');
+    if (linksEl.style.display === 'block') {
+      linksEl.style.display = 'none';
+    } else {
+      linksEl.style.display = 'block';
+    }
   }
-}
+});
 
-export default App;
+ReactDOM.render(<NavComponent />, document.querySelector('navbar'));
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+*/
