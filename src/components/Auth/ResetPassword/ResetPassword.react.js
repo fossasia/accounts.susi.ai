6712dccb5d624d0fd3140e2e5244185247e88771
DravitLochan <dlochangupta@gmail.com>
2017-07-04 22:42:21 +0530

    temporary

--- src/components/Auth/ResetPassword/ResetPassword.react.js
+++ src/components/Auth/ResetPassword/ResetPassword.react.js
@@ -7,10 +7,9 @@ import './ResetPassword.css';
 import AppBar from 'material-ui/AppBar';
 import PasswordField from 'material-ui-password-field';
 import { Link } from 'react-router-dom';
-// import Dialog from 'material-ui/Dialog';
-// import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 // import PropTypes from 'prop-types';
-// import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import UserPreferencesStore from '../../../stores/UserPreferencesStore';
 // import Login from '../Login/Login.react';
 
@@ -36,8 +35,117 @@ export default class ResetPassword extends Component{
 	handleSubmit = (event) => {
 		event.preventDefault();
 	}
+	handleChange = (event) => {
+			let email;
+			let password;
+			let confirmPassword;
+			let serverUrl;
+			let state = this.state
+			if (event.target.name === 'email') {
+					email = event.target.value.trim();
+					let validEmail =
+							/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+					state.email = email;
+					state.isEmail = validEmail;
+					state.emailError = !(email && validEmail)
+			}
+			else if (event.target.name === 'password') {
+					password = event.target.value;
+					let validPassword = password.length >= 6;
+					state.passwordValue = password;
+					state.passwordError = !(password && validPassword);
+			}
+			else if (event.target.name === 'confirmPassword') {
+					password = this.state.passwordValue;
+					confirmPassword = event.target.value;
+					let validPassword = confirmPassword === password;
+					state.confirmPasswordValue = confirmPassword;
+					state.passwordConfirmError = !(validPassword && confirmPassword);
+			}
+			else if (event.target.value === 'customServer') {
+					state.checked = true;
+					state.serverFieldError = true;
+			}
+			else if (event.target.value === 'standardServer') {
+					state.checked = false;
+					state.serverFieldError = false;
+			}
+			else if (event.target.name === 'serverUrl'){
+					serverUrl = event.target.value;
+					let validServerUrl =
+/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:~+#-]*[\w@?^=%&amp;~+#-])?/i
+					.test(serverUrl);
+					state.serverUrl = serverUrl;
+					state.serverFieldError = !(serverUrl && validServerUrl);
+			}
+
+			if (!this.state.emailError
+					&& !this.state.passwordError
+					&& !this.state.passwordConfirmError) {
+					state.validForm = true;
+			}
+			else {
+					state.validForm = false;
+			}
+
+			this.setState(state);
+
+			if (this.state.emailError) {
+					this.emailErrorMessage = 'Enter a valid Email Address';
+			}
+			else if (this.state.passwordError) {
+					this.emailErrorMessage = '';
+					this.passwordErrorMessage
+							= 'Minimum 6 characters required';
+					this.passwordConfirmErrorMessage = '';
+
+			}
+			else if (this.state.passwordConfirmError) {
+					this.emailErrorMessage = '';
+					this.passwordErrorMessage = '';
+					this.passwordConfirmErrorMessage = 'Check your password again';
+			}
+			else {
+					this.emailErrorMessage = '';
+					this.passwordErrorMessage = '';
+					this.passwordConfirmErrorMessage = '';
+			}
+
+			if (this.state.serverFieldError) {
+					this.customServerMessage = 'Enter a valid URL';
+			}
+			else{
+					this.customServerMessage = '';
+			}
+
+			if(this.state.emailError||
+			this.state.passwordError||
+			this.state.passwordConfirmError||
+			this.state.serverFieldError){
+					this.setState({validForm: false});
+			}
+			else{
+					this.setState({validForm: true});
+			}
+	}
 
 	render(){
+		const serverURL = <TextField name="serverUrl"
+												onChange={this.handleChange}
+												value={this.state.serverUrl}
+												errorText={this.customServerMessage}
+												floatingLabelText="Custom URL" />;
+
+		const hidden = this.state.checked ? serverURL : '';
+
+		const radioButtonStyles = {
+			block: {
+				maxWidth: 250,
+			},
+			radioButton: {
+				marginBottom: 16,
+			},
+		};
 		const styles = {
 			'margin': '60px auto',
 			'padding': '10px',
@@ -77,7 +185,6 @@ export default class ResetPassword extends Component{
 									// value={this.state.email}
 									onChange={this.handleChange} />
 							</div>
-
 							<div>
 								<PasswordField
 									name="newPassword"
@@ -98,6 +205,31 @@ export default class ResetPassword extends Component{
 							</div>
 						</form>
 						<br/>
+						<div>
+							<RadioButtonGroup style={{display: 'flex',
+								marginTop: '10px',
+								maxWidth:'200px',
+								flexWrap: 'wrap',
+								margin: 'auto'}}
+								name="server" onChange={this.handleChange}
+								defaultSelected="standardServer">
+								<RadioButton
+									value="customServer"
+									label="Custom Server"
+									labelPosition="left"
+									style={radioButtonStyles.radioButton}
+								/>
+								<RadioButton
+									 value="standardServer"
+									 label="Standard Server"
+									 labelPosition="left"
+									 style={radioButtonStyles.radioButton}
+								/>
+							</RadioButtonGroup>
+						</div>
+						<div>
+							{hidden}
+						</div>
 						<div>
 							<Link to={'/'}>
 								<RaisedButton
