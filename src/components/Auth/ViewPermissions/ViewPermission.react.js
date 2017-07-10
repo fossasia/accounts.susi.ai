08c117da3e812377548f4dcafd5c4090cf0f8f23
jyoth1raditya <jyothir_aditya@outlook.com>
2017-07-10 18:25:35 +0530

    Made UI changes

--- src/components/Auth/ViewPermissions/ViewPermission.react.js
+++ src/components/Auth/ViewPermissions/ViewPermission.react.js
@@ -3,10 +3,11 @@ import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 // import $ from 'jquery';
-import './ResetPassword.css';
+import './ViewPermission.css';
 import AppBar from 'material-ui/AppBar';
 import PasswordField from 'material-ui-password-field';
 import { Link } from 'react-router-dom';
+import $ from 'jquery';
 // import Dialog from 'material-ui/Dialog';
 // import FlatButton from 'material-ui/FlatButton';
 // import PropTypes from 'prop-types';
@@ -19,55 +20,102 @@ export default class ResetPassword extends Component{
 		super(props);
 
 		this.state={
-			email: '',
+			user: '',
 			msg: '',
-			currentPassword:'',
-			newPassword:'',
-			confirmPassword:'',
+			userRole: '',
 			success: false,
-			serverUrl: '',
-			checked:false,
-			serverFieldError: false,
-			emailError: true,
-			validEmail:true,
-			validForm:false
+			anonymous : true
 		};
 	}
-	handleSubmit = (event) => {
-		event.preventDefault();
+
+	ComponentDidMount(){
+		let ViewPermissionEndPoint ='api.susi.ai/aaa/account-permissions.json';
+		$.ajax({
+			url: ViewPermissionEndPoint,
+			dataType: 'jsonp',
+			jsonpCallback: 'p',
+			jsonp: 'callback',
+			crossDomain: true,
+			success: function (response) {
+				let state = this.state;
+				state.success = true;
+				let user = response.userName;
+				let anonymous = response.identity.anonymous;
+				let userRole = response.userRole;
+				state.user = user;
+				state.anonymous = anonymous;
+				state.userRole = userRole;
+				this.setState(state);
+				console.log(user);
+
+			}.bind(this),
+			error: function (errorThrown) {
+				let msg = 'Login Failed. Try Again';
+				let state = this.state;
+				state.msg = msg;
+				this.setState(state);
+			}.bind(this)
+		});
+
+
 	}
 
 	render(){
 		const styles = {
 			'margin': '60px auto',
-			'padding': '10px',
+			'padding': '0px',
 			'textAlign': 'center'
 		}
+		const styles2 = {
+			'textAlign': 'left'
+		}
+
 		return(
 			<div>
-				<div>
-					<AppBar
-						className="app-bar"
-						iconElementLeft={<iconButton></iconButton>}
-						style={{ backgroundColor : '#607D8B',
-							height: '46px' }}
-							titleStyle={{height:'46px'}}
-					/>
+			<div className="app-bar-div">
+			<header className='message-thread-heading'>
+										<AppBar className="app-bar"
+										iconElementLeft= {<iconButton></iconButton>}
+												style={{ backgroundColor : '#607D8B',
+														 height: '46px'}}
+												titleStyle={{height:'46px'}}
+										/>
+				</header>
 				</div>
-				<div className = "permissions">
-					<Paper zDepth={1} style={styles}>
+				<Paper zDepth={0}style={styles}>
+				<div className="permissions">
 						<h1>View Permissions</h1>
-						<br/>
-						<div class="section" id="userSpecific">
+						<div class="user-permission" id="userSpecific">
 						<h2>Account information</h2>
-						<h4>User name:</h4>
+						<h4>User name: </h4>
 						<h4>User specific permissions:</h4>
 						<h4>User role:</h4>
 						<h4>Parent user role:</h4>
 						<h4>User role specific permissions:</h4>
 						<br/>
-						</Paper>
+					  </div>
+						<div class="user-services" id="userSpecific">
+						<h2>List of services</h2>
+						<h4>StatusService: </h4>
+						<h4>AppsService: </h4>
+						<h4>LoginService: </h4>
+						<h4>AuthorizationDemoService: </h4>
+						<h4>PasswordRecoveryService: </h4>
+						<h4>PasswordResetService: </h4>
+						<h4>PublicKeyRegistrationService: </h4>
+						<h4>DownloadDataSettings: </h4>
+						<h4>SignUpService: </h4>
+						<h4>TopMenuService: </h4>
+						<h4>PasswordChangeService: </h4>
+						<h4>ListSettingsService: </h4>
+						<h4>ConvertSkillJsonToTxtService: </h4>
+
+						<br/>
+
 				</div>
+				</div>
+			</Paper>
+			</div>
 		);
 	}
 }
