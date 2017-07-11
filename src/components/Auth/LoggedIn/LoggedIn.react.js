9e3f5600671b61380e92a1966fc222bf34e08808
Jyothir Aditya <jyothir_aditya@outlook.com>
2017-07-11 23:02:48 +0530

    Implemented Logout in Homepage (#109)
    * Enu is not poping up
    
    * Finished Logout button

--- src/components/Auth/LoggedIn/LoggedIn.react.js
+++ src/components/Auth/LoggedIn/LoggedIn.react.js
@@ -2,22 +2,39 @@ import React, { Component } from 'react';
 import './LoggedIn.css';
 import AppBar from 'material-ui/AppBar';
 import { slide as Menu } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
+import FlatButton from 'material-ui/FlatButton';
+/* eslint-disable */
+class Logout extends Component {
+  static muiName = 'FlatButton';
+
+  render() {
+    return (
+      <Link to={'/logout'} ><FlatButton style={{color: 'white' }}
+      className="logout-btn" label="Logout" /></Link>
+    );
+  }
+}
 
 class LoggedIn extends Component {
   /* constructor(props){
     super(props);
   } */
-  render () {
+
+
+  render (){
     return(
       <div>
       <div className='app-bar-div'>
-        <AppBar
-        iconElementLeft={<iconButton></iconButton>}
-            className="app-bar"
-            style={{ backgroundColor : '#607D8B',
-                 height: '46px'}}
-            titleStyle={{height:'46px'}}
-        />
+      <AppBar
+      iconElementLeft= {<iconButton></iconButton>}
+          className="app-bar"
+          style={{ backgroundColor : '#607D8B',
+               height: '46px'}}
+          titleStyle={{height:'46px'}}
+        iconElementRight={<Logout />}
+
+      />
       </div>
 
       <div>
@@ -37,5 +54,4 @@ class LoggedIn extends Component {
     )
   }
 }
-
 export default LoggedIn;
