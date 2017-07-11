92a83aa2c96a61bedb96ee7e3c5292b4d05a4b9f
jyoth1raditya <jyothir_aditya@outlook.com>
2017-07-11 13:44:27 +0530

    Enu is not poping up

--- src/components/Auth/LoggedIn/LoggedIn.react.js
+++ src/components/Auth/LoggedIn/LoggedIn.react.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
 import './LoggedIn.css';
 import AppBar from 'material-ui/AppBar';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from 'react-router-dom';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
+class ListMenu extends Component {
+    render(){
+        return (
+          <IconMenu className='IconMenu'
+  					iconButtonElement={
+  						<IconButton iconStyle={{ fill: 'white' }}>
+  							<MoreVertIcon /></IconButton>
+  					}
+  					targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+  					anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+  				   >
+  					<MenuItem primaryText="Logout"
+  						containerElement={<Link to="/logout" />} />
+              <MenuItem primaryText="Settings" />
+              <MenuItem primaryText="Help" />
+              <MenuItem primaryText="Sign out" />
+  				</IconMenu>
+
+        );
+    }
+}
 
 class LoggedIn extends Component {
   /* constructor(props){
     super(props);
   } */
-  render () {
+
+
+  render (){
     return(
       <div className='app-bar-div'>
-        <AppBar
-        iconElementLeft={<iconButton></iconButton>}
-            className="app-bar"
-            style={{ backgroundColor : '#607D8B',
-                 height: '46px'}}
-            titleStyle={{height:'46px'}}
-        />
+      <AppBar
+      iconElementLeft= {<iconButton></iconButton>}
+          className="app-bar"
+          style={{ backgroundColor : '#607D8B',
+               height: '46px'}}
+          titleStyle={{height:'46px'}}
+        iconElementRight={<ListMenu />}
+
+      />
       </div>
     )
   }
 }
-
 export default LoggedIn;
