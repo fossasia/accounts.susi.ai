829c46813a91d7679deda2ce12df917717418cf4
Dravit Lochan <DravitLochan@users.noreply.github.com>
2017-07-12 22:40:10 +0530

    added option to change password (#110)
    * change password ajax call
    
    * refactor
    
    * AJAX call for change password and UI restructuring
    
    * added reset password link in navigation bar
    
    * bug fix
    
    * renamed chat to accounts in package.json
    
    * Change Password Added

--- src/components/Auth/LoggedIn/LoggedIn.react.js
+++ src/components/Auth/LoggedIn/LoggedIn.react.js
@@ -36,7 +36,6 @@ class LoggedIn extends Component {
 
       />
       </div>
-
       <div>
       <Menu  customBurgerIcon={ <img alt="" key="sidebar-icon" src="img/icon.svg" />} />
        <Menu customCrossIcon={ <img alt="sidebar-cross"
@@ -46,11 +45,22 @@ class LoggedIn extends Component {
         <ul><a id="Chat" className="menu-item" href="http://chat.susi.ai">Chat with susi</a></ul>
         <ul><a id="SusiSkillCMS" className="menu-item" href="">Susi skill CMS</a></ul>
         <ul><a id="ViewPermission" className="menu-item" >View Permissions</a></ul>
-        <ul><a id="ChangePassword" className="menu-item" >Change Password</a></ul>
+        <ul><Link to = '/changepassword'><a id="ChangePassword" className="menu-item" >Change Password</a></Link></ul>
         </li>
        </Menu>
        </div>
-       </div>
+      {/* <div>
+            <Link to={'/changepassword'}>
+            <RaisedButton
+              label='change password'
+              backgroundColor={
+                  UserPreferencesStore.getTheme()==='light'
+                  ? '#607D8B' : '#19314B'}
+              labelColor='#fff'/>
+              <h3></h3>
+              </Link>
+            </div> */}
+      </div>
     )
   }
 }
