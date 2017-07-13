9a9831359a254153c8190fb344e6598f4e1d9741
jyoth1raditya <jyothir_aditya@outlook.com>
2017-07-13 12:23:29 +0530

    Initial commit

--- src/components/Auth/LoggedIn/LoggedIn.react.js
+++ src/components/Auth/LoggedIn/LoggedIn.react.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import './LoggedIn.css';
 import AppBar from 'material-ui/AppBar';
-import { slide as Menu } from 'react-burger-menu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import RaisedButton from 'material-ui/RaisedButton';
 import { Link } from 'react-router-dom';
-import FlatButton from 'material-ui/FlatButton';
-/* eslint-disable */
-class Logout extends Component {
-  static muiName = 'FlatButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
-  render() {
-    return (
-      <Link to={'/logout'} ><FlatButton style={{color: 'white' }}
-      className="logout-btn" label="Logout" /></Link>
-    );
-  }
-}
+const ListMenu = () => (
+          <IconMenu className='IconMenu'
+                      iconButtonElement={
+                          <IconButton iconStyle={{ fill: 'white' }}>
+                              <MoreVertIcon /></IconButton>
+                      }
+                      targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                      anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                     >
+              <MenuItem primaryText="Logout"
+                    containerElement={<Link to="/logout" />} />
+              <MenuItem primaryText="Settings" />
+              <MenuItem primaryText="Help" />
+              <MenuItem primaryText="Sign out" />
+                  </IconMenu>
+
+
+);
 
 class LoggedIn extends Component {
   /* constructor(props){
@@ -24,7 +35,6 @@ class LoggedIn extends Component {
 
   render (){
     return(
-      <div>
       <div className='app-bar-div'>
       <AppBar
       iconElementLeft= {<iconButton></iconButton>}
@@ -32,34 +42,9 @@ class LoggedIn extends Component {
           style={{ backgroundColor : '#607D8B',
                height: '46px'}}
           titleStyle={{height:'46px'}}
-        iconElementRight={<Logout />}
+        iconElementRight={<ListMenu />}
 
       />
-      </div>
-      <div>
-      <Menu  customBurgerIcon={ <img alt="" key="sidebar-icon" src="img/icon.svg" />} />
-       <Menu customCrossIcon={ <img alt="sidebar-cross"
-                            key="cross" src="img/cross.svg" /> } />
-      <Menu className="menu-new">
-      <li>
-        <ul><a id="Chat" className="menu-item" href="http://chat.susi.ai">Chat with susi</a></ul>
-        <ul><a id="SusiSkillCMS" className="menu-item" href="">Susi skill CMS</a></ul>
-        <ul><a id="ViewPermission" className="menu-item" >View Permissions</a></ul>
-        <ul><Link to = '/changepassword'><a id="ChangePassword" className="menu-item" >Change Password</a></Link></ul>
-        </li>
-       </Menu>
-       </div>
-      {/* <div>
-            <Link to={'/changepassword'}>
-            <RaisedButton
-              label='change password'
-              backgroundColor={
-                  UserPreferencesStore.getTheme()==='light'
-                  ? '#607D8B' : '#19314B'}
-              labelColor='#fff'/>
-              <h3></h3>
-              </Link>
-            </div> */}
       </div>
     )
   }
