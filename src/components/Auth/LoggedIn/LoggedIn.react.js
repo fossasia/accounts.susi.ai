bdbdc564f25e33ac1a46c727b30691413dff6cba
Jyothir Aditya <jyothir_aditya@outlook.com>
2017-07-18 16:01:54 +0530

    [Open]Added Menu List to accounts (#112)
    * Initial commit
    
    * List menu is working
    
    * fixed bug
    
    * Fixed as per suggestions

--- src/components/Auth/LoggedIn/LoggedIn.react.js
+++ src/components/Auth/LoggedIn/LoggedIn.react.js
@@ -1,65 +1,48 @@
 import React, { Component } from 'react';
 import './LoggedIn.css';
 import AppBar from 'material-ui/AppBar';
-import { slide as Menu } from 'react-burger-menu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
 import { Link } from 'react-router-dom';
-import FlatButton from 'material-ui/FlatButton';
-/* eslint-disable */
-class Logout extends Component {
-  static muiName = 'FlatButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
-  render() {
-    return (
-      <Link to={'/logout'} ><FlatButton style={{color: 'white' }}
-      className="logout-btn" label="Logout" /></Link>
-    );
-  }
-}
+const ListMenu = () => (
+          <IconMenu className='IconMenu'
+                      tooltip="Options"
+                      iconButtonElement={
+                          <IconButton
+                          className='menu-icon'
+                          iconStyle={{ fill : 'white',}}>
+                              <MoreVertIcon /></IconButton>
+                      }
+                      targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                      anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                     >
+                     <MenuItem primaryText="Chat With Susi"
+ 										href="http://chat.susi.ai" />
+                     <MenuItem primaryText="Change Password"
+                           containerElement={<Link to="/changepassword" />} />
+                    <MenuItem primaryText="Logout"
+                    containerElement={<Link to="/logout" />} />
+                  </IconMenu>
 
+);
 class LoggedIn extends Component {
-  /* constructor(props){
-    super(props);
-  } */
 
 
   render (){
     return(
-      <div>
       <div className='app-bar-div'>
       <AppBar
-      iconElementLeft= {<iconButton></iconButton>}
+      iconElementLeft={<iconButton ></iconButton>}
           className="app-bar"
           style={{ backgroundColor : '#607D8B',
                height: '46px'}}
           titleStyle={{height:'46px'}}
-        iconElementRight={<Logout />}
+        iconElementRight={<ListMenu />}
 
       />
-      </div>
-      <div>
-      <Menu  customBurgerIcon={ <img alt="" key="sidebar-icon" src="img/icon.svg" />} />
-       <Menu customCrossIcon={ <img alt="sidebar-cross"
-                            key="cross" src="img/cross.svg" /> } />
-      <Menu className="menu-new">
-      <li>
-        <ul><a id="Chat" className="menu-item" href="http://chat.susi.ai">Chat with susi</a></ul>
-        <ul><a id="SusiSkillCMS" className="menu-item" href="">Susi skill CMS</a></ul>
-        <ul><a id="ViewPermission" className="menu-item" >View Permissions</a></ul>
-        <ul><Link to = '/changepassword'><a id="ChangePassword" className="menu-item" >Change Password</a></Link></ul>
-        </li>
-       </Menu>
-       </div>
-      {/* <div>
-            <Link to={'/changepassword'}>
-            <RaisedButton
-              label='change password'
-              backgroundColor={
-                  UserPreferencesStore.getTheme()==='light'
-                  ? '#607D8B' : '#19314B'}
-              labelColor='#fff'/>
-              <h3></h3>
-              </Link>
-            </div> */}
       </div>
     )
   }
