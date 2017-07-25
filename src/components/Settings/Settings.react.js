f68592ad3c3601d9c7beb351c436dcbfd2e74340
DravitLochan <dlochangupta@gmail.com>
2017-07-25 22:02:07 +0530

    Settings UI

--- src/components/Settings/Settings.react.js
+++ src/components/Settings/Settings.react.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 import './Settings.css';
+import AppBar from 'material-ui/AppBar';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
-import Sidebar from '../Utils/Sidebar.react'
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+  backgroundColor: '#607D8B'
+};
 
 class Settings extends Component {
 
   render(){
     return(
       <div>
-        <Sidebar />
+        <div className="app-bar-div">
+          <AppBar
+            className="app-bar"
+            iconElementLeft={<iconButton></iconButton>}
+            style={{ backgroundColor : '#607D8B',
+              height: '46px' }}
+            titleStyle={{height:'46px'}}
+          />
+        </div>
+        <div className="SettingsTab">
+          <Tabs>
+            <Tab label="Android" >
+              <div>
+                <h2 style={styles.headline}>Settings for Android</h2>
+              </div>
+            </Tab>
+            <Tab label="iOS" >
+              <div>
+                <h2 style={styles.headline}>Settings for iOS</h2>
+              </div>
+            </Tab>
+            <Tab label="Web Chat">
+              <div>
+                <h2 style={styles.headline}>Settings for Web Chat</h2>
+              </div>
+            </Tab>
+          </Tabs>
+        </div>
       </div>
     )
   }
