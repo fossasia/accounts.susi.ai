666b804490ee6a8bbe07a79955e9412c267fd28c
Rishi Raj <rishiraj824@gmail.com>
2017-08-22 15:15:49 +0530

    fixed travis failure

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,12 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out ../deploy_key -d
+openssl aes-256-cbc -K $encrypted_1cc96214ae7e_key -iv $encrypted_1cc96214ae7e_iv -in deploy_key.enc -out deploy_key -d
 
+
+
+
+   
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
 ssh-add ../deploy_key
