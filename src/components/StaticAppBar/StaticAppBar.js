c50fcfc0916b956667ccfa3ae459e9e2b3e39682
Anshuman <anshu.av97@gmail.com>
2018-05-28 14:28:09 +0530

    rm Popover component

--- src/components/StaticAppBar/StaticAppBar.js
+++ src/components/StaticAppBar/StaticAppBar.js
@@ -8,7 +8,6 @@ import Cookies from 'universal-cookie';
 import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
-import Popover from 'material-ui/Popover';
 
 // Static assets
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
