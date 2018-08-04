6000ef8cfa540ca17542784311a3383422718c95
akshatnitd <coolakshat24@gmail.com>
2018-08-04 12:10:23 +0530

    Fixes #405: Add avatar on Static App Bar

--- src/Utils/urls.js
+++ src/Utils/urls.js
@@ -3,6 +3,7 @@ let urls = {
   CHAT_URL: 'https://chat.susi.ai',
   CMS_URL: 'https://skills.susi.ai',
   ACC_URL: 'https://accounts.susi.ai',
+  GRAVATAR_URL: 'https://www.gravatar.com/avatar',
 };
 
 export default urls;
--- src/components/StaticAppBar/StaticAppBar.js
+++ src/components/StaticAppBar/StaticAppBar.js
@@ -22,9 +22,10 @@ import Exit from 'material-ui/svg-icons/action/exit-to-app';
 import LoginIcon from 'material-ui/svg-icons/action/account-circle';
 import List from 'material-ui/svg-icons/action/list';
 import susiWhite from '../../images/susi-logo-white.png';
-import './StaticAppBar.css';
-import { urls, isProduction } from '../../Utils';
+import CircleImage from '../CircleImage/CircleImage';
 import ChatConstants from '../../constants/ChatConstants';
+import { urls, isProduction, getAvatarProps } from '../../Utils';
+import './StaticAppBar.css';
 
 const cookieDomain = isProduction() ? '.susi.ai' : '';
 
@@ -104,31 +105,43 @@ class StaticAppBar extends Component {
   }
 
   render() {
+    const isLoggedIn = !!cookies.get('loggedIn');
+    let avatarProps = null;
+    if (isLoggedIn) {
+      avatarProps = getAvatarProps(cookies.get('emailId'));
+    }
+
     let ListMenu = props => (
-      <div>
-        {cookies.get('loggedIn') ? (
-          <label
+      <div className="topRightMenu">
+        {isLoggedIn && (
+          <div
             style={{
-              color: 'white',
-              fontSize: '16px',
-              verticalAlign: 'super',
-              position: 'relative',
-              top: '-8px',
-              right: '-8px',
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginTop: '-14px',
             }}
           >
-            {cookies.get('username') === ''
-              ? cookies.get('emailId')
-              : cookies.get('username')}
-          </label>
-        ) : (
-          <label />
+            <CircleImage {...avatarProps} size="32" />
+            <label
+              style={{
+                color: 'white',
+                marginRight: '5px',
+                fontSize: '16px',
+              }}
+            >
+              {cookies.get('username') === '' ||
+              cookies.get('username') === 'undefined'
+                ? cookies.get('emailId')
+                : cookies.get('username')}
+            </label>
+          </div>
         )}
         <IconMenu
           className="IconMenu"
           animated={false}
+          style={{ top: '-2px' }}
           tooltip="Options"
-          style={{ right: '-8px', top: '-2px' }}
           iconButtonElement={
             <IconButton className="menu-icon" iconStyle={{ fill: 'white' }}>
               <MoreVertIcon />
@@ -223,7 +236,6 @@ class StaticAppBar extends Component {
             height: '46px',
           }}
           titleStyle={{ height: '46px' }}
-          iconStyleRight={{ marginRight: '-8px', marginTop: '7px' }}
           iconElementRight={<ListMenu />}
         />
       </header>
