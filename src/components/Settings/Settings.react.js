a96ab88e99d557264ea79483b7164b97b3b2efdb
neeraj3029 <31539812+neeraj3029@users.noreply.github.com>
2018-09-25 23:27:35 +0530

    account deactivation link alignment fixed (#507)
    deactivation link aligned in footer
    
    alignment of deactivation link fixed

--- src/components/Settings/Settings.react.js
+++ src/components/Settings/Settings.react.js
@@ -1511,6 +1511,7 @@ class Settings extends Component {
                     style={{
                       textAlign: 'center',
                       marginTop: '20px',
+                      marginBottom: '20px',
                       display: 'flex',
                       justifyContent: 'center',
                     }}
