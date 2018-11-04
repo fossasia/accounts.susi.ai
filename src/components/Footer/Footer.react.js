2324daf9e053b8bfa0a67c30b6fd925f2cc4325c
Shubham Gupta <shubhamg@outlook.com>
2018-11-04 21:00:31 +0530

    Make stateless components into functional components #572

--- src/components/Footer/Footer.react.js
+++ src/components/Footer/Footer.react.js
@@ -1,41 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import susi from '../../images/susi-logo.svg';
 import './Footer.css';
 import { Link } from 'react-router-dom';
 import { urls } from '../../Utils';
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      video: false,
-    };
-  }
-  render() {
-    // Footer Component
-    return (
-      <div className="footer">
-        <div className="footer-container">
-          <Link to="/">
-            <img src={susi} alt="SUSI" className="susi-logo" />
-          </Link>
-          <ul className="alignLeft">
-            <li>
-              <a href={`${urls.CHAT_URL}/overview`}>Overview</a>
-            </li>
-            <li>
-              <a href={`${urls.CHAT_URL}/blog`}>Blog</a>
-            </li>
-            <li>
-              <a href={`${urls.API_URL}`}>API</a>
-            </li>
-            <li>
-              <a href="https://github.com/fossasia?utf8=%E2%9C%93&q=susi">
-                Code
-              </a>
-            </li>
-          </ul>
-          <ul className="alignRight">
+const Footer = () => {
+  // Footer Component
+  return (
+    <div className="footer-wrap">
+      <div className="footer-container">
+        <Link className="susi-logo-container" to="/">
+          <img src={susi} alt="SUSI" className="susi-logo-footer" />
+        </Link>
+        <div className="footer-content">
+          <div className="footer-left">
+            <ul>
+              <li>
+                <a href={`${urls.CHAT_URL}/overview`}>Overview</a>
+              </li>
+              <li>
+                <a href={`${urls.CHAT_URL}/blog`}>Blog</a>
+              </li>
+              <li>
+                <a href={`${urls.API_URL}`}>API</a>
+              </li>
+              <li>
+                <a href="https://github.com/fossasia?utf8=%E2%9C%93&q=susi">
+                  Code
+                </a>
+              </li>
+            </ul>
+          </div>
+          <ul>
             <li>
               <a href={`${urls.CHAT_URL}/contact`}>Contact</a>
             </li>
@@ -48,8 +44,8 @@ class Footer extends Component {
           </ul>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Footer;
