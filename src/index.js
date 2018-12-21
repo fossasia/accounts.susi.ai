874a6e1cc9f2ab970ae69cfe47afad57c185203d
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-12-21 18:12:18 +0530

    Fixes #624: Snackbar declared, offline status feature implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  hashHistory,
+} from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Snackbar from 'material-ui/Snackbar';
+
 import SignUp from './components/Auth/SignUp/SignUp.react';
 import Logout from './components/Auth/Logout.react';
 import ChangePassword from './components/Auth/ChangePassword/ChangePassword.react';
@@ -14,16 +25,6 @@ import Login from './components/Auth/Login/Login.react';
 import NotFound from './components/NotFound/NotFound.react';
 
 import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  hashHistory,
-} from 'react-router-dom';
 
 const styles = {
   app: {
@@ -32,32 +33,110 @@ const styles = {
   },
 };
 
-const App = () => (
-  <Router history={hashHistory}>
-    <MuiThemeProvider>
-      <div style={styles.app}>
-        <div id="outer-container">
-          <main id="page-wrap" />
-        </div>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/changepassword" component={ChangePassword} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/logout" component={Logout} />
-          <Route exact path="/settings" component={Settings} />
-          <Route exact path="/admin" component={Admin} />
-          <Route exact path="/admin/users" component={Users} />
-          <Route exact path="/admin/skills" component={Skills} />
-          <Route exact path="/admin/settings" component={SystemSettings} />
-          <Route exact path="/admin/logs" component={SystemLogs} />
-          <Route exact path="/verify-account" component={VerifyAccount} />
-          <Route exact path="/resetpass" component={ResetPassword} />
-          <Route exact path="/delete-account" component={DeleteAccount} />
-          <Route exact path="*" component={NotFound} />
-        </Switch>
-      </div>
-    </MuiThemeProvider>
-  </Router>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      snackBarOpen: false,
+      snackBarMessage: '',
+      snackBarDuration: 4000,
+      snackBarAction: null,
+      snackBarActionHandler: null,
+    };
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('offline', this.onUserOffline);
+    window.addEventListener('online', this.onUserOnline);
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener('offline', this.onUserOffline);
+    window.removeEventListener('online', this.onUserOnline);
+  };
+
+  onUserOffline = () => {
+    this.openSnackBar({
+      snackBarMessage: 'It seems you are offline!',
+    });
+  };
+
+  onUserOnline = () => {
+    this.openSnackBar({
+      snackBarMessage: 'Welcome back!',
+    });
+  };
+
+  openSnackBar = ({
+    snackBarMessage,
+    snackBarDuration = 4000,
+    snackBarActionHandler,
+    snackBarAction,
+  }) => {
+    this.setState({
+      snackBarOpen: true,
+      snackBarMessage,
+      snackBarDuration,
+      snackBarActionHandler,
+      snackBarAction,
+    });
+  };
+
+  closeSnackBar = () => {
+    this.setState({
+      snackBarOpen: false,
+      snackBarMessage: '',
+      snackBarDuration: 4000,
+      snackBarAction: null,
+      snackBarActionHandler: null,
+    });
+  };
+
+  render() {
+    const {
+      snackBarOpen,
+      snackBarMessage,
+      snackBarDuration,
+      snackBarAction,
+      snackBarActionHandler,
+    } = this.state;
+
+    return (
+      <Router history={hashHistory}>
+        <MuiThemeProvider>
+          <div style={styles.app}>
+            <div id="outer-container">
+              <main id="page-wrap" />
+            </div>
+            <Snackbar
+              autoHideDuration={snackBarDuration}
+              action={snackBarAction}
+              onActionTouchTap={snackBarActionHandler}
+              open={snackBarOpen}
+              message={snackBarMessage}
+              onRequestClose={this.closeSnackBar}
+            />
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/changepassword" component={ChangePassword} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/logout" component={Logout} />
+              <Route exact path="/settings" component={Settings} />
+              <Route exact path="/admin" component={Admin} />
+              <Route exact path="/admin/users" component={Users} />
+              <Route exact path="/admin/skills" component={Skills} />
+              <Route exact path="/admin/settings" component={SystemSettings} />
+              <Route exact path="/admin/logs" component={SystemLogs} />
+              <Route exact path="/verify-account" component={VerifyAccount} />
+              <Route exact path="/resetpass" component={ResetPassword} />
+              <Route exact path="/delete-account" component={DeleteAccount} />
+              <Route exact path="*" component={NotFound} />
+            </Switch>
+          </div>
+        </MuiThemeProvider>
+      </Router>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
